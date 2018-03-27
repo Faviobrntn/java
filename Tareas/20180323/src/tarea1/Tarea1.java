a537faf24029fea8e274e9a27053851633d192bb
Favio <favio@syloper.com>
2018-03-26 22:18:04 -0300

    correccion

--- Tareas/20180323/src/tarea1/Tarea1.java
+++ Tareas/20180323/src/tarea1/Tarea1.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Tarea1 {
 
-	private static Scanner s;
-
 	public static void main(String[] args) {
-
+		Scanner s = new Scanner(System.in);
 		String[] palabras = new String[20];
 		
 		for (int i = 0; i < palabras.length; i++) {
-			System.out.println("Palabra "+(i+1)+": ");
-			s = new Scanner(System.in);
-			String palab = s.nextLine();
-			palabras[i] = palab;
+			System.out.print("Palabra "+(i+1)+": ");
+			palabras[i] = s.nextLine();
 		}
 		
 		for (int i = palabras.length-1; i >= 0 ; i--) {
